a4829a7afcabb4e8966a72fbabc042927884b3a6
Edgariukstis <edgaras.banaitis@gmail.com>
2020-09-08 08:42:20 +0300

    bad migrations

---
+++ database/migrations/2020_09_07_152632_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->integer('customers', 6);
+            $table->integer('employees', 4);
+            $table->integer('menu_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
